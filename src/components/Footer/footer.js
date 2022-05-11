3b83ad4cf2104527b32fdc44d0e3f883f1c1d7e7
kyuri Park <kraprk058@gmail.com>
2022-05-11 15:42:11 +0900

    commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <h1>Footer</h1>
+        )
+    }
+}
+export default Footer;
---
+++ src/components/Main/main.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+
+class Main extends Component{
+    render(){
+        return(
+            <h1>Main</h1>
+        )
+    }
+}
+export default Main;
